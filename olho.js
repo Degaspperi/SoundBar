eb1c07f5728b791d99a0b8786c0c263781c39a71
Degaspperi <degasperiarthur@gmail.com>
2023-03-14 20:24:38 -0300

    Escopo inicial do projeto

---
+++ olho.js
@@ -0,0 +1,12 @@
+const olho = document.getElementById("olho");
+olho.addEventListener("click", function () {
+  showHide();
+});
+function showHide() {
+  const password = document.getElementById("password");
+  if (password.type == "password") {
+    password.setAttribute("type", "text");
+  } else {
+    password.setAttribute("type", "password");
+  }
+}
