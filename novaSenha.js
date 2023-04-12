9ffcb7d1158bc611ba637215e01b159b91dd7d41
Arthur Degasperi <degasperiarthur@gmail.com>
2023-04-12 20:53:39 -0300

    Atualizações

---
+++ novaSenha.js
@@ -0,0 +1,5 @@
+const botaoEntrar = document.getElementById("botaoEntrar")
+
+botaoEntrar.addEventListener("click", function (){
+    window.location.href = ("file:///D:/VsProgramas/Soundbar/index.html")
+})
